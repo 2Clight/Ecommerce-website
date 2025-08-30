ff6fcbff870265f7bdc09c5e708ab40531b40ab1
Abyalew Teklu <abyalewteklu9@gmail.com>
2025-08-30 15:30:41 +0530

    First commit

---
+++ backend/lib/redis.js
@@ -0,0 +1,15 @@
+import { createClient } from "redis"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+export const redis = createClient({
+  url: process.env.UPSTASH_REDIS_URL
+});
+
+redis.on("error", function(err) {
+  throw err;
+});
+await redis.connect()
+await redis.set('foo','bar');
+
---
+++ backend/model/User.js
@@ -0,0 +1,70 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: 6
+    },
+
+    cartItems: [{
+        quantity: {
+            type: Number,
+            default: 1
+        },
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    }],
+
+    role: {
+        type: String,
+        enum: ['customer', 'admin'],
+        default: 'customer'
+    },
+}, { timestamps: true }); //creates createdAt and updatedAt fields
+
+
+
+//hashing password before saving to database using bcrypt.js
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next(); //a callback to indicate that the middleware is done and to proceed to the next middleware in the stack
+    }
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next(); //with no arguments indicates success, and saves the document
+    }
+    catch (err) {
+        next(err); //with an error argument indicates failure, and aborts the save operation
+    }
+}); 
+
+
+//method to compare entered password with hashed password in database
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+//placed last because it is the final step in the model definition
+const User= mongoose.model("User", userSchema);
+
+
+
+export default User;
